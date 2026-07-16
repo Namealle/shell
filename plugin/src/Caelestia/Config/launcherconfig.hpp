352a5cae2864b9fc68f8f9123be2d75a65755b63
Namealle <gachknonazar2009@gmail.com>
2026-07-15 20:12:40 -0400

    launcher: clipboard picker (; prefix) C++ fuzzy ranking

--- plugin/src/Caelestia/Config/launcherconfig.hpp
+++ plugin/src/Caelestia/Config/launcherconfig.hpp
@@ -31,6 +31,8 @@ class LauncherConfig : public settings::ObjectNode {
     CONFIG_PROPERTY(int, maxWallpapers, 9)
     CONFIG_GLOBAL_PROPERTY(QString, specialPrefix, u"@"_s)
     CONFIG_GLOBAL_PROPERTY(QString, actionPrefix, u">"_s)
+    CONFIG_GLOBAL_PROPERTY(QString, clipboardPrefix, u";"_s)
+    CONFIG_GLOBAL_PROPERTY(QString, emojiPrefix, u":"_s)
     CONFIG_GLOBAL_PROPERTY(bool, enableDangerousActions, false)
     CONFIG_PROPERTY(int, dragThreshold, 50)
     CONFIG_GLOBAL_PROPERTY(bool, vimKeybinds, false)
